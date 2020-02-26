56ab8e671fdd57957d9e2193d066b42128cec594
CaryDai <daiqianjieaixia@126.com>
2020-02-26 10:44:23 +0800

    修改了部分注释内容

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # RPC_V1
 RPC版本一
-使用 Socket + BIO
+使用 Socket传输 + Java序列化 + BIO 实现。
--- src/hdu/dqj/RPCServer/Service/HelloServiceImpl.java
+++ src/hdu/dqj/RPCServer/Service/HelloServiceImpl.java
@@ -1,14 +1,13 @@
 package hdu.dqj.RPCServer.Service;
 
-import java.io.Serializable;
-
 /**
  * @Author dqj
  * @Date 2019/12/3
  * @Version 1.0
- * @Description Remote端接口的实现。需要实现Serializable接口，序列化要使用。
+ * @Description Remote端接口的实现。
  */
-public class HelloServiceImpl implements HelloService, Serializable {
+public class HelloServiceImpl implements HelloService {
+    // 方法返回的String类型实现了 java.io.Serializable 接口，因此能被序列化。
     @Override
     public String sayHi(String name) {
         return "Hi," + name;
